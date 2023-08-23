341b96ab1dd757e8a37157485aa36f87a3c17ab1
Akash <akashikur011@gmail.com>
2023-08-23 22:43:15 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setErrorMessage("Both username and password are required.");
+    } else {
+      setErrorMessage("");
+      // Here you can perform your login logic
+    }
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+        {errorMessage && <p id="errorMessage">{errorMessage}</p>}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
